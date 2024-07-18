318086198711f162a43c419dd652fd2a26747e29
Arnold Zhou <magicarnold2022@gmail.com>
2024-07-19 08:19:38 +1000

    feat: add breadcrumb for admin page

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { getUsers } from "../services/AdminService";
 import { useAuth } from "../contexts/AuthContext";
-import Container from "react-bootstrap/Container";
 import { UserRole } from "@mrmagic2020/shared/dist/enums";
+import Breadcrumb from "react-bootstrap/Breadcrumb";
+import Container from "react-bootstrap/Container";
 
 const UserList: React.FC = () => {
   const role = useAuth().role;
@@ -22,10 +23,16 @@ const UserList: React.FC = () => {
   }, []);
 
   if (role !== UserRole.Admin) {
-    return <Container fluid>You are not authorized to view this page</Container>;
+    return (
+      <Container fluid>You are not authorized to view this page</Container>
+    );
   }
   return (
     <Container fluid>
+      <Breadcrumb>
+        <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+        <Breadcrumb.Item active>Admin</Breadcrumb.Item>
+      </Breadcrumb>
       <h2>Registered Users</h2>
       <ul>
         {users.map((user: any) => (
