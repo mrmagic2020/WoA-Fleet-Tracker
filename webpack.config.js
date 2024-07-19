547fb19901e2e6513fe9ce78454a51f8fdfde96e
Arnold Zhou <magicarnold2022@gmail.com>
2024-07-19 12:23:23 +1000

    fix: favicon not showing

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -27,7 +28,12 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html'
+      template: "./public/index.html"
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "public", to: "", globOptions: { ignore: ["**/index.html"] } }
+      ]
     })
   ],
   devServer: {
