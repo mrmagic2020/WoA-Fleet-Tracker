92111d09d77879be26528ba8c580810c400d5a07
Arnold Zhou <magicarnold2022@gmail.com>
2024-07-20 12:34:09 +1000

    feat: improve login and registration UI

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -6,12 +6,14 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { Link } from "react-router-dom";
 
 const Login: React.FC = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoggingIn, setIsLoggingIn] = useState(false);
   const navigate = useNavigate();
   const { isAuthenticated, login: loginContext } = useAuth();
 
@@ -21,49 +23,65 @@ const Login: React.FC = () => {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setIsLoggingIn(true);
     try {
       const { user } = await login(username, password);
       loginContext(user);
       navigate("/");
-    } catch (err) {
+    } catch (err: any) {
       console.error("Failed to login", err);
-      alert("Login failed");
+      alert(`Login failed: ${err.response.data.message}`);
+    } finally {
+      setIsLoggingIn(false);
     }
   };
 
   return (
     <Container fluid>
-      <Form onSubmit={handleSubmit}>
-        <h2>Login</h2>
-        <br />
-        <Col xs={3}>
-          <FloatingLabel controlId="floatingInput" label="Username">
-            <Form.Control
-              type="username"
-              value={username}
-              placeholder="Username"
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-          </FloatingLabel>
-          <br />
-          <FloatingLabel controlId="floatingPassword" label="Password">
-            <Form.Control
-              type="password"
-              value={password}
-              placeholder="Password"
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </FloatingLabel>
+      <Row className="min-vh-100">
+        <Col xs={12} md={6} lg={4}>
+          <Form className="p-4 border rounded shadow" onSubmit={handleSubmit}>
+            <h2>Login</h2>
+            <br />
+            <FloatingLabel
+              controlId="floatingInput"
+              label="Username"
+              className="mb-3"
+            >
+              <Form.Control
+                type="username"
+                value={username}
+                placeholder="Username"
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </FloatingLabel>
+            <FloatingLabel
+              controlId="floatingPassword"
+              label="Password"
+              className="mb-3"
+            >
+              <Form.Control
+                type="password"
+                value={password}
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </FloatingLabel>
+            <div className="d-flex justify-content-between align-items-center">
+              <Link to="/register">Register</Link>
+              <Button
+                variant="outline-primary"
+                disabled={isLoggingIn}
+                type="submit"
+              >
+                {isLoggingIn ? "Logging in..." : "Login"}
+              </Button>
+            </div>
+          </Form>
         </Col>
-        <br />
-        <Link to="/register">Register</Link>
-        <br />
-        <Button variant="outline-primary" type="submit">
-          Login
-        </Button>
-      </Form>
+      </Row>
     </Container>
   );
 };
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -6,6 +6,7 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { Link } from "react-router-dom";
 
@@ -13,6 +14,7 @@ const Register: React.FC = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [invitationCode, setInvitationCode] = useState("");
+  const [isRegistering, setIsRegistering] = useState(false);
   const navigate = useNavigate();
   const { isAuthenticated, login: loginContext } = useAuth();
 
@@ -22,6 +24,7 @@ const Register: React.FC = () => {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setIsRegistering(true);
     try {
       const { token, user } = await register(
         username,
@@ -34,54 +37,69 @@ const Register: React.FC = () => {
     } catch (err: any) {
       console.error("Failed to register", err);
       alert(`Registration failed: ${err.response.data.message}`);
+    } finally {
+      setIsRegistering(false);
     }
   };
 
   return (
     <Container fluid>
-      <Form onSubmit={handleSubmit}>
-        <h2>Register</h2>
-        <br />
-        <Col xs={3}>
-          <FloatingLabel controlId="floatingInput" label="Username">
-            <Form.Control
-              type="username"
-              value={username}
-              placeholder="Username"
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-          </FloatingLabel>
-          <br />
-          <FloatingLabel controlId="floatingPassword" label="Password">
-            <Form.Control
-              type="password"
-              value={password}
-              placeholder="Password"
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </FloatingLabel>
-          <br />
-          <FloatingLabel
-            controlId="floatingInvitationCode"
-            label="Invitation Code"
-          >
-            <Form.Control
-              type="invitationCode"
-              value={invitationCode}
-              placeholder="Invitation Code"
-              onChange={(e) => setInvitationCode(e.target.value)}
-            />
-          </FloatingLabel>
+      <Row className="min-vh-100">
+        <Col xs={12} md={6} lg={4}>
+          <Form className="p-4 border rounded shadow" onSubmit={handleSubmit}>
+            <h2>Register</h2>
+            <br />
+            <FloatingLabel
+              controlId="floatingInput"
+              label="Username"
+              className="mb-3"
+            >
+              <Form.Control
+                type="username"
+                value={username}
+                placeholder="Username"
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </FloatingLabel>
+            <FloatingLabel
+              controlId="floatingPassword"
+              label="Password"
+              className="mb-3"
+            >
+              <Form.Control
+                type="password"
+                value={password}
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </FloatingLabel>
+            <FloatingLabel
+              controlId="floatingInvitationCode"
+              label="Invitation Code"
+              className="mb-3"
+            >
+              <Form.Control
+                type="invitationCode"
+                value={invitationCode}
+                placeholder="Invitation Code"
+                onChange={(e) => setInvitationCode(e.target.value)}
+              />
+            </FloatingLabel>
+            <div className="d-flex justify-content-between align-items-center">
+              <Link to="/login">Login</Link>
+              <Button
+                variant="outline-primary"
+                disabled={isRegistering}
+                type="submit"
+              >
+                {isRegistering ? "Registering..." : "Register"}
+              </Button>
+            </div>
+          </Form>
         </Col>
-        <br />
-        <Link to="/login">Login</Link>
-        <br />
-        <Button variant="outline-primary" type="submit">
-          Register
-        </Button>
-      </Form>
+      </Row>
     </Container>
   );
 };
