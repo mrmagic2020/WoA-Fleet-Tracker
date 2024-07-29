b87d1842d914c559fea62ba8f54dafeabd1dce73
Arnold Zhou <magicarnold2022@gmail.com>
2024-07-29 16:46:38 +1000

    feat: add images for available aircrafts

--- src/components/AircraftDetailsListGroup.tsx
+++ src/components/AircraftDetailsListGroup.tsx
@@ -3,8 +3,11 @@ import {
   IAircraft,
   IAircraftContract
 } from "@mrmagic2020/shared/dist/interfaces";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ListGroup from "react-bootstrap/ListGroup";
+import Image from "react-bootstrap/Image";
 import Currency from "./Currency";
 
 interface AircraftDetailsListGroupProps {
@@ -17,37 +20,51 @@ const AircraftDetailsListGroup: React.FC<AircraftDetailsListGroupProps> = ({
   customItems
 }) => {
   return (
-    <Col xs="auto">
-      <ListGroup>
-        <ListGroup.Item>Model: {aircraft.ac_model}</ListGroup.Item>
-        <ListGroup.Item>Size: {aircraft.size}</ListGroup.Item>
-        <ListGroup.Item>Type: {aircraft.type}</ListGroup.Item>
-        <ListGroup.Item>Registration: {aircraft.registration}</ListGroup.Item>
-        <ListGroup.Item>Airport: {aircraft.airport}</ListGroup.Item>
-        <ListGroup.Item>Status: {aircraft.status}</ListGroup.Item>
-        <ListGroup.Item>
-          Total Contracts: {aircraft.contracts.length}
-        </ListGroup.Item>
-        <ListGroup.Item>
-          Total Income:{" "}
-          <Currency
-            value={aircraft.contracts.reduce(
-              (acc: number, contract: IAircraftContract) =>
-                acc +
-                contract.profits.reduce(
-                  (acc: number, profit: number) => acc + profit,
+    <Container fluid>
+      <Row>
+        <Col>
+          <ListGroup>
+            <ListGroup.Item>Model: {aircraft.ac_model}</ListGroup.Item>
+            <ListGroup.Item>Size: {aircraft.size}</ListGroup.Item>
+            <ListGroup.Item>Type: {aircraft.type}</ListGroup.Item>
+            <ListGroup.Item>
+              Registration: {aircraft.registration}
+            </ListGroup.Item>
+            <ListGroup.Item>Airport: {aircraft.airport}</ListGroup.Item>
+            <ListGroup.Item>Status: {aircraft.status}</ListGroup.Item>
+            <ListGroup.Item>
+              Total Contracts: {aircraft.contracts.length}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Total Income:{" "}
+              <Currency
+                value={aircraft.contracts.reduce(
+                  (acc: number, contract: IAircraftContract) =>
+                    acc +
+                    contract.profits.reduce(
+                      (acc: number, profit: number) => acc + profit,
+                      0
+                    ),
                   0
-                ),
-              0
-            )}
+                )}
+              />
+            </ListGroup.Item>
+            {customItems &&
+              customItems.map((item, index) => (
+                <ListGroup.Item key={index}>{item}</ListGroup.Item>
+              ))}
+          </ListGroup>
+        </Col>
+        <Col xs="auto">
+          <Image
+            width={400}
+            src={`/assets/aircraft/${aircraft.ac_model}.jpeg`}
+            alt={aircraft.ac_model}
+            rounded
           />
-        </ListGroup.Item>
-        {customItems &&
-          customItems.map((item, index) => (
-            <ListGroup.Item key={index}>{item}</ListGroup.Item>
-          ))}
-      </ListGroup>
-    </Col>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
